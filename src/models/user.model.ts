9ea26d1c5b1c2374d0856bc84e663425be62e7b3
Rohit Singh Rawat <rohitzrawat2003@gmail.com>
2024-04-28 23:14:03 +0530

    feat: Add bcrypt and jwt

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import jwt, { Secret } from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 interface UserDocument extends mongoose.Document {
 	username: string;
 	email: string;
@@ -56,6 +58,43 @@ const userSchema: mongoose.Schema<UserDocument> = new mongoose.Schema(
 	}
 );
 
+userSchema.pre('save', async function (next) {
+	if (!this.isModified('password')) return next();
+
+	const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password, salt);
+	return next();
+});
+
+userSchema.methods.comparePassword = async function(userPassword: string)  {
+	return await bcrypt.compare(userPassword, this.password);
+};
+
+userSchema.methods.generateAccessToken =  function(){
+	return jwt.sign(
+		{
+			_id: this._id,
+			email: this.email,
+			username: this.username,
+			fullName: this.fullName,
+		},
+		process.env.ACCESS_TOKEN_SECRET as Secret,
+		{
+			expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+		}
+	);
+}
+userSchema.methods.generateRefreshToken = function () {
+	return jwt.sign(
+		{
+			_id: this._id,
+		},
+		process.env.REFRESH_TOKEN_SECRET as Secret,
+		{
+			expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+		}
+	);
+};
 const User: mongoose.Model<UserDocument> = mongoose.model<UserDocument>(
 	'User',
 	userSchema
