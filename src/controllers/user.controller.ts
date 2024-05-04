c998e0417f36f95fc8b118a25aa0a62049dd1378
Rohit Singh Rawat <rohitzrawat2003@gmail.com>
2024-05-04 11:55:30 +0530

    Feat: complete user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -380,3 +380,56 @@ export const getUserChannelProfile = asyncHandler(async (req, res) => {
 		);
 });
 
+export const getWatchHistory = asyncHandler(async (req, res) => {
+	const user = await User.aggregate([
+		{
+			$match: {
+				_id: req.user._id,
+			},
+		},
+		{
+			$lookup: {
+				from: 'videos',
+				foreignField: '_id',
+				localField: 'watchHistory',
+				as: 'watchHistory',
+				pipeline: [
+					{
+						$lookup: {
+							from: 'users',
+							foreignField: '_id',
+							localField: 'owner',
+							as: 'owner',
+							pipeline: [
+								{
+									$project: {
+										fullName: 1,
+										avatar: 1,
+										username: 1,
+									},
+								},
+							],
+						},
+					},
+					{
+						$addFields: {
+							owner: {
+								$first: '$owner',
+							},
+						},
+					},
+				],
+			},
+		},
+	]);
+
+	return res
+		.status(200)
+		.json(
+			new ApiResponse(
+				200,
+				user[0].watchHistory,
+				'watch history fetched successfully'
+			)
+		);
+});
